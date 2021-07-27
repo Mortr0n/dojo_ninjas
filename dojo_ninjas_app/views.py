f8cd744810f0124a9ae16c8ea59e3071fa2b42b7
Chris <cmsl4y3r@gmail.com>
2021-07-26 20:12:57 -0600

    Dojos Table entry complete, Dojos and ninjas listing corrected

--- dojo_ninjas_app/views.py
+++ dojo_ninjas_app/views.py
@@ -5,10 +5,13 @@ from .models import Dojos, Ninjas
 def index(request):
     context={
         "all_dojos" : Dojos.objects.all(),
-        "all_ninjas" : Ninjas.objects.all()
+        "all_ninjas" : Ninjas.objects.all(),
+        
     }
     return render(request, 'index.html', context)
 
 def add_dojo(request):
-    
-    redirect('/')
+    new_dojo = Dojos.objects.create(name=request.POST['name_input'], city=request.POST['city_input'], state=request.POST['state_input'], desc = request.POST['desc_input'])
+    return redirect('/')
+
+
